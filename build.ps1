5d1fe755b8441e11bc8a96b8cfd1bc60d8d11a7a
s2quake <han0210@netsgo.com>
2020-08-29 21:54:29 +0900

    빌드 스크립트 개선

--- build.ps1
+++ build.ps1
@@ -22,10 +22,17 @@
 $location = Get-Location
 try {
     Set-Location $PSScriptRoot
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s2quake/build/master/build.ps1" -OutFile .\.vscode\build.ps1
-    .\.vscode\build.ps1 -WorkingPath $PSScriptRoot -PropsPath "Directory.Build.props"
+    $buildFile = "./.vscode/build.ps1"
+    $propsPath = (
+        "../JSSoft.Library/Directory.Build.props",
+        "./Directory.Build.props"
+    ) | ForEach-Object { "`"$_`"" }
+    $propsPath = $propsPath -join ","
+    $solutionPath = "./JSSoft.Library.Commands.sln"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s2quake/build/master/build.ps1" -OutFile $buildFile
+    Invoke-Expression "$buildFile $solutionPath $propsPath $args"
 }
 finally {
-    Remove-Item .\.vscode\build.ps1
+    Remove-Item ./.vscode/build.ps1
     Set-Location $location
 }
