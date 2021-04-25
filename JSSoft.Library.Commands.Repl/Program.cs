a238e40ee5f0317658a25a84b2cc4b78c25266b4
s2quake <s2quake@naver.com>
2021-04-25 21:50:38 +0900

    linux환경에서 디렉터리 생성후 attributes 설정에 예외가 발생하는 부분을 수정

--- JSSoft.Library.Commands.Repl/Program.cs
+++ JSSoft.Library.Commands.Repl/Program.cs
@@ -28,6 +28,8 @@ namespace JSSoft.Library.Commands.Repl
     {
         static async Task Main(string[] _)
         {
+            var info = new System.IO.DirectoryInfo("/Users/s2quake-mac/Projects/workspace/repository/.repository/users/Members");
+            info.Create();
             var shell = Container.GetService<IShell>();
             Console.WriteLine();
             await shell.StartAsync();
